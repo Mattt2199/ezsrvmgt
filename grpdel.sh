66fed0abc3557b13588bfc2facbf3ed8d66f104d
Mattt2199 <155832860+Mattt2199@users.noreply.github.com>
2024-12-23 18:01:53 +0100

    Create grpdel.sh

---
+++ grpdel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------------------------------------------------
+#Delete a group if exist
+echo "Insert group name:"
+read groupname
+# Check if group already exist
+if sudo getent group "$groupname" >/dev/null; then
+	# Generate error if group exist and explain to the user which is the problem
+	echo "-------------------- ERROR --------------------"
+	echo "GROUP $groupname ALREADY EXIST!"
+	echo "-----------------------------------------------"
+else
+	# If gorup does not exist, create the group
+	sudo groupadd $groupname
+	echo "$groupname correctly added!"
+fi
