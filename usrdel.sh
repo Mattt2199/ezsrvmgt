773e1d7f5dcacf21172fb91d3fb447b64d753c07
Mattt2199 <155832860+Mattt2199@users.noreply.github.com>
2024-12-23 17:56:22 +0100

    Create usrdel.sh

---
+++ usrdel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------------------------------------------------
+#Delete user if exist
+echo "Insert username:"
+read deleteusername
+# Check if user exist
+if sudo id -u "$deleteusername" >/dev/null 2>&1; then
+# If user exist delete the machine and the samba user but don't delete the user folder
+	sudo userdel -f $deleteusername
+	sudo smbpasswd -x $deleteusername
+	echo "$deleteusername correctly removed!"
+else
+# Generate error if user does not exist and explain to the user which is the problem
+	echo "-------------------- ERROR --------------------"
+	echo "USER $deleteusername DOES NOT EXIST!"
+	echo "-----------------------------------------------"
+fi
