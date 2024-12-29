d8c3dcc2c53f29b89804b2e224c22f6978cb0bbd
Mattt2199 <155832860+Mattt2199@users.noreply.github.com>
2024-12-29 14:35:13 +0100

    Update usrdel.sh

--- usrdel.sh
+++ usrdel.sh
@@ -8,7 +8,6 @@ if sudo id -u "$deleteusername" >/dev/null 2>&1; then
 # If user exist delete the machine and the samba user but don't delete the user folder
 	sudo userdel -f $deleteusername
 	sudo smbpasswd -x $deleteusername
-	sudo rm -rf /ezsrvshare/$deleteusername
 	echo "$deleteusername correctly removed!"
 else
 # Generate error if user does not exist and explain to the user which is the problem
